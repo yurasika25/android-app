7e6ffb0925522379c7724c300600eb5789e1b90e
yurasika25 <yrasika80@gmail.com>
2020-07-15 12:23:24 +0300

    + new charche

--- app/src/main/java/ru/startandroid/develop/testapp/Repository.java
+++ app/src/main/java/ru/startandroid/develop/testapp/Repository.java
@@ -3,6 +3,7 @@ package ru.startandroid.develop.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Repository extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class Repository extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repository);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
