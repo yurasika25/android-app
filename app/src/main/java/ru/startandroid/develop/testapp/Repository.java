79180a810fc854baefecbc416f83c7bd6f1c46af
yurasika25 <yrasika80@gmail.com>
2020-07-15 12:21:30 +0300

    new repository

---
+++ app/src/main/java/ru/startandroid/develop/testapp/Repository.java
@@ -0,0 +1,14 @@
+package ru.startandroid.develop.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Repository extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository);
+    }
+}
